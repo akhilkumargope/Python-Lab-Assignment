f119cbfef2d8002842d1828c2682e2b546337248
akhilkumargope <akhil.gope@mca.christuniversity.in>
2024-07-28 16:43:45 +0530

    Update 1.py

--- Lab Ex 1/1.py
+++ Lab Ex 1/1.py
@@ -2,25 +2,35 @@ a=[1,2,3,4,5,6]
 b=["akhil","tony","batman",]
 
 #Sum of the elements
-print(sum(a))
+def add(a):
+    print(sum(a))
 
 #Product of the elements
-p=1
-for i in range(len(a)):
-    p= p*a[i]
-print(p)
+def product(a):
+    p=1
+    for i in range(len(a)):
+        p= p*a[i]
+    print(p)
 
 #Largest of all
-max=a[i]
-for i in range(len(a)-1):
-    if(a[i] > max):
-        max=a[i]
-print(max)
+def largest(a):
+    max=a[0]
+    for i in range(len(a)):
+        if(a[i] > max):
+            max=a[i]
+    print(max)
 
 
 #Minimum of all
-min=a[i]
-for i in range(len(a)-1):
-    if(a[i] < min):
-        min=a[i]
-print(min)
+def minimun(a):
+    min=a[0]
+    for i in range(len(a)):
+        if(a[i] < min):
+            min=a[i]
+    print(min)
+
+
+add(a)
+product(a)
+largest(a)
+minimun(a)
