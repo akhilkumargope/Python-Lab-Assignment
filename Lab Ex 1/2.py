b508dc8ee07a71ad8fa1a809f49bb31d9470129c
akhilkumargope <akhil.gope@mca.christuniversity.in>
2024-07-28 16:56:03 +0530

    Update 2.py

--- Lab Ex 1/2.py
+++ Lab Ex 1/2.py
@@ -1,1 +1,5 @@
-a=["abc", "xyz", "aba", "1221"]
+A=['abc', 'xyz', 'aba', '1221']
+
+for itr in A :
+    if itr[0] == itr[len(itr)-1] :
+        print(f"Identical String {itr} found at index {A.index(itr)}")
